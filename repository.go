ca67d9d9fc3f5764aa881e34fc5ebb95f50dd634
Andreas Buechele <andreas@buechele.io>
2022-04-29 16:21:45 +0200

    rename and restructure repository

--- repository.go
+++ repository.go
@@ -9,40 +9,41 @@ import (
 	"strings"
 )
 
-type BaseRepository[T any] interface {
-	GetDetail(id int) (*T, error)
-	GetList() (*[]T, error)
-	Search(criteria entity.Criteria) (*[]T, error)
-	SearchIds(criteria entity.Criteria) (*[]string, error)
-}
-
-type Repository[T any] struct {
-	endpoints  Endpoints
+type EntityRepository[T any] struct {
+	endpoints  endpoints
 	httpClient *http.Client
 }
 
-type Endpoints struct {
+type RepositoryConfiguration struct {
+	apiBaseUrl string
+	httpClient *http.Client
+}
+
+type endpoints struct {
 	entity    string
 	search    string
 	searchIds string
 }
 
-// CreateRepository Creates a new generic repository. The Shopware 6 base API URL and a preconfigured
+// CreateEntityRepository Creates a new generic repository. The Shopware 6 base API URL and a preconfigured
 // http.Client is needed.
-func CreateRepository[T any](apiBaseUrl string, httpClient *http.Client) *Repository[T] {
+func CreateEntityRepository[T any](config *RepositoryConfiguration) *EntityRepository[T] {
 	var a T
 	endpoint := strings.ToLower(toKebabCase(reflect.TypeOf(a).Name()))
-	client := httpClient
-	entityEndPoint := strings.TrimRight(apiBaseUrl, "/") + "/" + endpoint
-	searchEndPoint := strings.TrimRight(apiBaseUrl, "/") + "/search/" + endpoint
-	searchIdsEndPoint := strings.TrimRight(apiBaseUrl, "/") + "/search-ids/" + endpoint
-	endpoints := Endpoints{entityEndPoint, searchEndPoint, searchIdsEndPoint}
-	return &Repository[T]{endpoints, client}
+	client := config.httpClient
+	entityEndPoint := strings.TrimRight(config.apiBaseUrl, "/") + "/" + endpoint
+	searchEndPoint := strings.TrimRight(config.apiBaseUrl, "/") + "/search/" + endpoint
+	searchIdsEndPoint := strings.TrimRight(config.apiBaseUrl, "/") + "/search-ids/" + endpoint
+	endpoints := endpoints{entityEndPoint, searchEndPoint, searchIdsEndPoint}
+	return &EntityRepository[T]{endpoints, client}
 }
 
-func (g *Repository[T]) GetDetail(id string) (*T, error) {
+func (g *EntityRepository[T]) GetDetail(id string) (*T, error) {
 	url := g.endpoints.entity + "/" + id
-	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
 	body, err := getBody(g.httpClient, req)
 	if body == nil || err != nil {
 		return nil, err
@@ -54,7 +55,7 @@ func (g *Repository[T]) GetDetail(id string) (*T, error) {
 	return &result.Data, nil
 }
 
-func (g *Repository[T]) GetList() (*[]T, error) {
+func (g *EntityRepository[T]) GetList() (*[]T, error) {
 	req, err := http.NewRequest(http.MethodGet, g.endpoints.entity, nil)
 	if err != nil {
 		return nil, err
@@ -62,20 +63,26 @@ func (g *Repository[T]) GetList() (*[]T, error) {
 	return queryArray[T](g.httpClient, req)
 }
 
-func (g *Repository[T]) Search(criteria entity.Criteria) (*[]T, error) {
+func (g *EntityRepository[T]) Search(criteria entity.Criteria) (*[]T, error) {
 	criteriaJSON, err := json.Marshal(criteria)
 	if err != nil {
 		return nil, err
 	}
-	req, _ := http.NewRequest(http.MethodPost, g.endpoints.search, bytes.NewBuffer(criteriaJSON))
+	req, err := http.NewRequest(http.MethodPost, g.endpoints.search, bytes.NewBuffer(criteriaJSON))
+	if err != nil {
+		return nil, err
+	}
 	return queryArray[T](g.httpClient, req)
 }
 
-func (g *Repository[T]) SearchIds(criteria entity.Criteria) (*[]string, error) {
+func (g *EntityRepository[T]) SearchIds(criteria entity.Criteria) (*[]string, error) {
 	criteriaJSON, err := json.Marshal(criteria)
 	if err != nil {
 		return nil, err
 	}
-	req, _ := http.NewRequest(http.MethodPost, g.endpoints.searchIds, bytes.NewBuffer(criteriaJSON))
+	req, err := http.NewRequest(http.MethodPost, g.endpoints.searchIds, bytes.NewBuffer(criteriaJSON))
+	if err != nil {
+		return nil, err
+	}
 	return queryArray[string](g.httpClient, req)
 }
